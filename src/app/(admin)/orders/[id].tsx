767ad8a14323294e89d24c0bc6bab13ffb3bbce0
Krishna Prasad Khanal <krishnaamen@gmail.com>
2024-04-16 14:54:42 +0200

    adding top tab navigator and mannaging orders active or archive for admin side

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -1,9 +1,11 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
 import orders from '@/assets/data/orders';
 import OrderListItem from '@/src/components/OrderListItem';
 import OrderItemListItem from '@/src/components/OrderItemListItem';
+import { OrderStatusList } from '@/src/types';
+import Colors from '@/src/constants/Colors';
 
 const OrderDetailsScreen = () => {
 
@@ -23,6 +25,40 @@ const OrderDetailsScreen = () => {
            <OrderItemListItem item={item} />
         )}
         contentContainerStyle={{  gap: 10, padding: 10 }} 
+        ListFooterComponent={() => (
+          <>
+  <Text style={{ fontWeight: 'bold' }}>Status</Text>
+  <View style={{ flexDirection: 'row', gap: 5 }}>
+    {OrderStatusList.map((status) => (
+      <Pressable
+        key={status}
+        onPress={() => console.warn('Update status')}
+        style={{
+          borderColor: Colors.light.tint,
+          borderWidth: 1,
+          padding: 10,
+          borderRadius: 5,
+          marginVertical: 10,
+          backgroundColor:
+            order.status === status
+              ? Colors.light.tint
+              : 'transparent',
+        }}
+      >
+        <Text
+          style={{
+            color:
+              order.status === status ? 'white' : Colors.light.tint,
+          }}
+        >
+          {status}
+        </Text>
+      </Pressable>
+    ))}
+  </View>
+</>
+
+        )}
         />
     
     </View>
